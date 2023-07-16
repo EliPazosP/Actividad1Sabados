1a7c9a44cf8218b3a00f110923f2c202fa32f054
EliPazosP <epazosqa@gmail.com>
2023-07-16 10:11:41 -0500

    Mi primer proyecto completo

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+
+
+function validaciondatos() {
+    let nombre = document.getElementById("name").value /* Se guarda en esta vairable el valor que viene en la propiedad nombre */
+    alert("Bienvenido al formulario " + nombre)
+}
